4a89d4e32ffce550e3328447b0a4355165a90614
Priyansh <electroniorje@gmail.com>
2026-03-02 15:10:06 +0530

    Version 1.0 commit

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ let getStartedBtn = document.querySelector(".getstarted-btn");
 let getStartedtop = document.querySelector(".getstarted");
 let contactBtn = document.querySelector(".contact-btn");
 let mobscreen = document.querySelector(".mobscreen-navbar");
+let linksMob = document.querySelector(".links-mobscreen");
 
 getStartedBtn.addEventListener("click", () => {
     window.location.href = "./flashcard.html";
@@ -16,5 +17,8 @@ contactBtn.addEventListener("click", () => {
 });
 
 mobscreen.addEventListener("click", ()=> {
-    
+    if(linksMob.style.display === "flex")
+        linksMob.style.display = "none";
+    else 
+        linksMob.style.display = "flex";
 });
